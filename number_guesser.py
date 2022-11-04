1a06e4a1bce1e78cb7540e4fb609f80092817c43
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2022-11-04 23:00:24 +0400

    Create number_guesser.py

---
+++ number_guesser.py
@@ -0,0 +1,30 @@
+import random
+
+n = int(input("Enter the end range: "))
+num = random.randint(0,n+1)
+
+guessed_numbers = []
+
+print("\nYou have 5 chances in total to guess the number from 0 to ",n)
+print()
+
+ans = 'y'
+while ans.lower() == 'y':
+	chances = 5
+	
+	while chances > 0:
+		user_num = int(input("Enter your guessed number: "))
+	
+		if num == user_num:
+			print("You have guessed the number correctly. Congrats")
+			exit()
+	
+		else:
+			guessed_numbers.append(user_num)
+			print("The entered number is not correct try again.")
+			print("\n")
+			chances-=1
+	
+	print("The number(s) entered is/are: ",guessed_numbers)
+	
+	ans = input("Do you wish to continue?(Y/n): ")
