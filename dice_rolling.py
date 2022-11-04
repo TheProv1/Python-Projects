0654b517ed9e2742b2510406f5c4fdce88ce4146
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2022-11-04 23:35:15 +0400

    dice_rolling.py
    This code simulates the "Rolling of a die/pair of dice", and displays the output of the face.

---
+++ dice_rolling.py
@@ -0,0 +1,38 @@
+import random
+
+def single_die():
+	'''
+	This function gives a random value of the die (1-6)
+	'''
+	
+	die_roll = random.randint(1,6)
+	return die_roll
+
+def double_dice():
+	'''
+	This function uses the previous function (single_die()) twice
+	'''
+	
+	val_1 = single_die()
+	val_2 = single_die()
+	return val_1, val_2
+
+num1, num2 = double_dice()
+
+sum_faces = n1 + n2
+
+print("\t\t\tMENU")
+print("\n1. One Die\n2. Two Dice\n")
+
+ans = 'y'
+while ans.lower() == 'y':
+	option = int(input("Enter your choice"))
+	
+	if option == 1:
+		print("The value of the face is: ",single_die())
+	
+	elif option == 2: 
+		print("The value of the first face is: ",num1, "\nand the value of the second face is: ",num2)
+		print("The sum of the faces is: ",sum_faces)
+		
+	ans = input("Do you wish to continue?(Y/n): ")
