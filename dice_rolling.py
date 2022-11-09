d0477d0fe485c35123482bfad1efb191da62b7bb
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2022-11-09 01:42:45 -0800

    Update dice_rolling.py
    Bug Fixes

--- dice_rolling.py
+++ dice_rolling.py
@@ -19,20 +19,23 @@ def double_dice():
 
 num1, num2 = double_dice()
 
-sum_faces = n1 + n2
+sum_faces = num1 + num2
 
 print("\t\t\tMENU")
 print("\n1. One Die\n2. Two Dice\n")
 
 ans = 'y'
 while ans.lower() == 'y':
-	option = int(input("Enter your choice"))
+	option = int(input("Enter your choice: "))
 	
 	if option == 1:
 		print("The value of the face is: ",single_die())
 	
 	elif option == 2: 
 		print("The value of the first face is: ",num1, "\nand the value of the second face is: ",num2)
+		print()
 		print("The sum of the faces is: ",sum_faces)
-		
+	
 	ans = input("Do you wish to continue?(Y/n): ")
+	print("\n")
+	
